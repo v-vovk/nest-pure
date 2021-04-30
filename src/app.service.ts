efaed36ddd80896b247e43d08ec806c97c0b294e
Vlad Vovk <wolfykey@gmail.com>
2021-04-30 18:34:14 +0300

    base init

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,12 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getUsers() {
+    return [
+      {
+        id: 1,
+        name: 'Vlad Vovk',
+      },
+    ];
   }
 }
